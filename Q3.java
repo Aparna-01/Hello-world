a29c78b7ac437f1122e125e7c64bf01caf3e09a1
Aparna-01 <84390712+Aparna-01@users.noreply.github.com>
2021-06-09 18:38:28 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,18 @@
+package task2;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // string to int
+		
+		
+		String s= "100";
+		
+		int i= Integer.parseInt(s);
+		
+		System.out.println(i);
+		
+	}
+
+}
---
+++ Task.java
@@ -0,0 +1,17 @@
+package task2;
+
+public class Task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// int to long 
+		
+		int number= 2147483647;
+		long num= number;
+		System.out.println(num);
+		
+		
+				
+	}
+
+}
